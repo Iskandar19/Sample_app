ecaf3df471fcf887e150c5a6241e172bb8d91810
Iskandar <walhamdulillah158@gmail.com>
2021-09-05 18:22:45 +0800

    Chapter9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params) # Not the final implementation!
     if @user.save
       log_in @user
-      flash[:success] = t "welcome_to_the_sample_app!"
+      flash[:success] = "Hello, Welcome to the Sample App!"
       redirect_to @user
       # Handle a successful save.
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   resources :users
   root 'static_pages#home'
   get 'static_pages/help'
