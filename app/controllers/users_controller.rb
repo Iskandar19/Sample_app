ac4c2fac721787301f24982531f93a12b494d33a
Iskandar <walhamdulillah158@gmail.com>
2021-09-05 16:03:34 +0800

    Chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
    def create
     @user = User.new(user_params) # Not the final implementation!
     if @user.save
+      log_in @user
       flash[:success] = t "welcome_to_the_sample_app!"
       redirect_to @user
       # Handle a successful save.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :users
   root 'static_pages#home'
   get 'static_pages/help'
@@ -6,4 +7,8 @@ Rails.application.routes.draw do
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
   resources :users, only: %i(new create)
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
