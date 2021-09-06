ca94b3a0ac336ec69ca8bde5bf37a849357cd6fa
Iskandar <walhamdulillah158@gmail.com>
2021-09-06 17:52:47 +0800

    chapter 10 manage users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save {self.email = email.downcase}
 
   validates :name, presence: true
@@ -6,16 +8,32 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
 
   # Returns the hash digest of the given string.
-  def User.digest string
+  def digest string
     cost = if ActiveModel::SecurePassword.min_cost
              BCrypt::Engine::MIN_COST
            else
              BCrypt::Engine.cost
            end
     BCrypt::Password.create string, cost: cost
+  end
+end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute :remember_digest, nil
   end
 
 end
